fa366f7d60073180b0d1154c14f636cdc02b1512
gabrielmonteirodev15 <gabrielmonteirodev15@gmail.com>
2025-05-23 17:05:17 -0300

    Resolucao do desafio

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioDeCodigo_Estacionamento.Models
+{
+    internal class Estacionamento(decimal x, decimal y)
+    {
+
+        List<string> veiculos = new List<string>();
+        public decimal entrada = x, valorHora = y;
+
+
+        public void AddVeiculo()
+        {
+            string? placa;
+            Console.WriteLine("Digite a placa do veiculo");
+
+
+            placa = Console.ReadLine();
+
+            if (placa != null)
+            {
+                veiculos.Add(placa);
+                Console.WriteLine("Veiculo adicionado com sucesso\n");
+            }
+            else
+            {
+                Console.WriteLine("Erro tente novamente \n");
+            }
+
+        }
+
+        public void RemoverVeiculo()
+        {
+            string? placa;
+            Console.WriteLine("Digite a placa do veiculo para remover");
+            placa = Console.ReadLine();
+
+            if (placa != null)
+            {
+                veiculos.Remove(placa);
+                Console.WriteLine("Veiculo removido com sucesso\n");
+            }
+            else
+            {
+                Console.WriteLine("Erro tente novamente \n");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            foreach (var item in veiculos)
+            {
+                Console.WriteLine($"Veiculo : {item}");
+            }
+        }
+
+        public void Encerrar()
+        {
+            Environment.Exit(0);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using DesafioDeCodigo_Estacionamento.Models;
+
+decimal entrada, valorHora;
+
+Console.WriteLine("Bem vindo ao sistema do estacionamento");
+Console.WriteLine("Qual o valor de entrada do estacionamento");
+
+entrada = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Qual o valor da hora");
+
+valorHora = Convert.ToDecimal(Console.ReadLine());
+
+Estacionamento e = new Estacionamento(entrada, valorHora);
+
+bool menu = true;
+
+while (menu)
+{
+    Console.WriteLine("Menu do estacionamento \n1-Adicionar veiculo \n2-Remover veiculo \n3-Listar veiculos \n4-Encerrar");
+
+
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            e.AddVeiculo();
+            break;
+
+        case "2":
+            e.RemoverVeiculo();
+            break;
+
+        case "3":
+            e.ListarVeiculos();
+            break;
+        case "4":
+            e.Encerrar();
+            break;
+
+        default:
+            Console.WriteLine("Entrada invalida");
+            break;
+    }
+}
